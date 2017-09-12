17d1d87de9fcf7aaf89a0a6738308ab27edea075
ZhipengHan <hanzhipeng@sensetime.com>
2017-09-12 11:25:51 +0800

    remote big box.
    Signed-off-by: ZhipengHan <hanzhipeng@sensetime.com>

---
+++ scripts/utils.py
@@ -0,0 +1,29 @@
+def remove_group_box(boxes, overlap_thresh=0.5, score_thresh=0.5, cover_box_num_thresh=5):
+  """
+    boxes: [[x, y, w, h, score], ...]
+  """
+
+  keep_boxes = []
+  removed_boxes = []
+
+  for box in boxes:
+    x1, y1, w, h, _ = box
+    x2, y2 = x1 + w - 1, y1 + h - 1
+    cover_box_num = 0
+
+    for box_t in boxes:
+      x1t, y1t, wt, ht, st = box_t
+      x2t, y2t = x1t + wt - 1, y1t + ht - 1
+      if box_t != box and st > score_thresh and max(x1, x1t) < min(x2, x2t) and max(y1, y1t) < min(y2, y2t):
+        overlap = (min(x2, x2t) - max(x1, x1t)) * (min(y2, y2t) - max(y1, y1t))
+        overlap = overlap / (wt * ht)
+        if overlap > overlap_thresh:
+          cover_box_num += 1
+
+    if cover_box_num >= cover_box_num_thresh:
+      removed_boxes.append(box)
+    else:
+      keep_boxes.append(box)
+
+  return keep_boxes, removed_boxes
+
---
+++ scripts/w.py
@@ -0,0 +1,39 @@
+from utils import remove_group_box
+
+filename = 'posetrack_trainval_nms03_sc05.txt'
+fout = open(filename + '_rmgroup.txt', 'wt')
+fout2 = open(filename + '_removed.txt', 'wt')
+
+dets = []
+im_name = ''
+
+
+with open(filename, 'rt') as f:
+  for line in f:
+    line = line[:-1]
+    cur_name, x1, y1, w, h, score = line.split(' ')
+    x1, y1, w, h, score = float(x1), float(y1), float(w), float(h), float(score)
+    if im_name == '':
+      im_name = cur_name
+    if cur_name != im_name and im_name != '':
+      keep_boxes = []
+      removed_boxes = []
+      keep_boxes, removed_boxes = remove_group_box(dets,score_thresh=0.4)
+      # print '====='
+      # print keep_boxes
+      # print(keep_boxes)
+      # print 1
+      for box in keep_boxes:
+        x1_, y1_, w_, h_, score_ = box
+        fout.write('%s %.2f %.2f %.2f %.2f %.5f\n' % (im_name, x1_, y1_, w_, h_, score_))
+      for box in removed_boxes:
+        x1_, y1_, w_, h_, score_ = box
+        fout2.write('%s %.2f %.2f %.2f %.2f %.5f\n' % (im_name, x1_, y1_, w_, h_, score_))
+      dets = []
+      im_name = cur_name
+      print(im_name)
+    dets.append([x1, y1, w, h, score])
+
+keep_boxes, removed_boxes = remove_group_box(dets,score_thresh=0.4)
+for box in keep_boxes:
+  fout.write('%s %.2f %.2f %.2f %.2f %.5f\n' % (im_name, x1, y1, w, h, score))
